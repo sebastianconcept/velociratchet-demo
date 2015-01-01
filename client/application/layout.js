3d7ab8fe8d1d856bd1e86b69e8d4bad2a873ac7a
Yacine Merzouk <yacine@merzouk.ca>
2014-12-31 17:42:40 -1000

    Updated demo for zendy:velociratchet@1.1.0

--- client/application/layout.js
+++ client/application/layout.js
@@ -1,30 +1,3 @@
-Template.layout.events({
-    'click': function(){
-        Velociratchet.transition = null;
-    },
-    'click .icon-right-nav': function(){
-        Session.set('previousPage', Router.current().route.getName() );
-        Velociratchet.transition = 'right-to-left';
-    },
-    'click .navigate-right': function(){
-        Session.set('previousPage', Router.current().route.getName() );
-        Velociratchet.transition = 'right-to-left';
-    },
-    'click .icon-left-nav': function(){
-        Session.set('previousPage', Router.current().route.getName() );
-        Velociratchet.transition = 'left-to-right';
-    },
-    'click .navigate-left': function(){
-        Session.set('previousPage', Router.current().route.getName() );
-        Velociratchet.transition = 'left-to-right';
-    }
-
-});
-
-Template.layout.helpers({
-    transition: function() {
-        return function(from, to, element) {
-            return Velociratchet.transition || 'fade';
-        }
-    }
-});
+// Velociratchet layout events & helpers
+Template.layout.events( Velociratchet.events );
+Template.layout.helpers( Velociratchet.helpers );
