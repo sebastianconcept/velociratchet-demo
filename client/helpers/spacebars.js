38cc403b7d80512a3b6f5172b5703a57d6ae58ba
Yacine Merzouk <yacine@merzouk.ca>
2015-02-02 12:04:12 -1000

    Fixed bugs introduced by refactoring file names in zendy:velociratchet package

--- client/helpers/spacebars.js
+++
@@ -1,9 +0,0 @@
-UI.registerHelper( 'getPreviousPage', function(){
-    return Session.get( 'previousPage' );
-});
-UI.registerHelper('isActive', function( args ) {
-    return args.hash.menu === args.hash.active ? 'active' : '';
-});
-UI.registerHelper('getCurrentRoute', function(  ) {
-    return Router.current().route.getName();
-});
